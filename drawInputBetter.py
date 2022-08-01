296965306a7f2faa126a0d46581ddd2e2cbba93c
Alex Yung <superay190@gmail.com>
2022-08-01 15:37:17 -0400

    fourth commit, updated drawinput

--- drawInputBetter.py
+++ drawInputBetter.py
@@ -2,6 +2,8 @@ import pygame
 import sys
 import cv2
 import os
+import time
+from djitellopy import Tello
 
 class UserStrokeInput:
     def __init__(self):
@@ -9,12 +11,15 @@ class UserStrokeInput:
         self.testimg2 = None
         self.pos1 = None
         self.pos2 = None
+        self.scalar = .05
+        self.timeConst = .3
     def input_image(self):
         pygame.init()
-        screen = pygame.display.set_mode((640, 480))
+        screen = pygame.display.set_mode((640, 640))
         pygame.display.set_caption("Insert Dance Move (Single Straight Line)")
         white_color = pygame.Color(255, 255, 255)
         blue_color = pygame.Color(0, 0, 255)
+        black_color = pygame.Color(0, 0, 0)
         screen.fill(white_color)
         loop = True
         self.pos1 = None
@@ -26,7 +31,7 @@ class UserStrokeInput:
                     self.pos1 = pygame.mouse.get_pos()
                 if loop == True and event.type == pygame.MOUSEBUTTONUP:
                     self.pos2 = pygame.mouse.get_pos()
-                    pygame.draw.line(screen, blue_color, self.pos1, self.pos2, 10)
+                    pygame.draw.line(screen, black_color, self.pos1, self.pos2, 10)
                     loop = False     
                 if loop == True and pygame.mouse.get_pressed()[0]:
                     try:
@@ -38,6 +43,7 @@ class UserStrokeInput:
             if(loop == False):
                 pygame.image.save(screen,"screenshot.jpg")
                 self.img = cv2.imread('screenshot.jpg', 0)
+                time.sleep(3)
                 pygame.quit()
                 break
             
@@ -52,13 +58,34 @@ class UserStrokeInput:
         cv2.imshow('grayscale image', self.img)
         cv2.waitKey(1000)
 
+    def convertInpToMove(self):
+        x1 = self.pos1[0]
+        y1 = 640 - self.pos1[1]
+        x2 = self.pos2[0]
+        y2 = 640 - self.pos2[1]
+        x_movement = self.scalar * (x2 - x1)
+        y_movement = self.scalar * (y2 - y1)
+        #cm/s
+        velx = x_movement/self.timeConst
+        vely = y_movement/self.timeConst
+
+        return (velx, vely)
+
+
+    def setScalar(self, input):
+        self.scalar = input
+
+    def setTime(self, input):
+        self.timeConst = input
 
 
 inp = UserStrokeInput()
 inp.input_image()
 inp.printImage()
 print(inp.getCoords())
-
-
+vals = inp.convertInpToMove()
+print(vals)
+# drone = Tello()
+# Tello.connect()
 # blueColor = (0, 0, 255)
 # pygame.display.update() #call everytime
