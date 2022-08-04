754a86503785441e208680b5efcb48d718399f90
avt05 <adrianvtran@gmail.com>
2022-08-04 11:32:30 -0400

    Fixed some dance moves, revised music subscribers

--- catkin_ws/src/buff_pkg/src/Driver.py
+++ catkin_ws/src/buff_pkg/src/Driver.py
@@ -5,50 +5,37 @@ from djitellopy import Tello
 import time
 import numpy as np
 from pygame import mixer
+from controlCenter import ControlCenter
+from challenges.msg import Song
+from challenges.msg import moves
+from challenges.msg import state
 
 class Driver(object):
 
     def __init__(self):
         rospy.init_node('driver', anonymous=True, disable_signals=True)
 
-        # sorry im terrible at ros so i definitely wrote these subscribers
-        # wrong, pretend they work properly
-        self.filename_sub = rospy.Subscriber('music/filename', notsure, self.filename_callback)
-        # subscribe to the file path of the song
-        self.pickup_sub = rospy.Subscriber('music/pickup', notsure, self.pickup_callback)
-        # subscribe to whether or not there is a pickup beat
-        self.tempo_sub = rospy.Subscriber('music/tempo', notsure, self.tempo_callback)
-        # subscribe to the tempo of the song
-        self.beatsPerMeasure = rospy.Subscriber('music/beatsPerMeasure', notsure, self.beatsPerMeasure_callback)
-        # subscribe to the number of beats per measure
-        self.beatTimes_sub = rospy.Subscriber('music/beatTime', notsure, self.beatTimes_callback)
-        # subscribe to the timestamps of each beat
-        self.beatDurations_sub = rospy.Subscriber('music/beatDuration', notsure, self.beatDuration_callback)
-        # subscribe to the durations of each beat
+        rospy.Subscriber('music/song', Song, self.data_callback)
+        rospy.Subscriber('tello/moves', ControlCenter, self.moves_callback())
         
 
-        # alex, make sure u subscribe to stuff from draw input so it can 
-        # receive the list of moves
+        ### we need to incorporate moves/state somehow, i'm not too sure how those work ###
 
-    def filename_callback(self, data):
-        self.filename = data        # listen to music input node, store file path of song
-    
-    def pickup_callback(self, data):
-        self.pickup = data          # listen to music input node, store whether or not there is pickup
-    
-    def tempo_callback(self, data):
-        self.tempo = data           # listen to music input node, store tempo
+    def data_callback(self, data):
+        self.data = data        # listen to music input node, store file path of song
+        self.filename = self.data.filename
+        self.tempo = self.data.tempo
+        self.beats_per_measure = self.data.time_sig
+        self.pickup = self.data.pickup
+        self.beat_durations = self.data.beat_durations
 
-    def beatsPerMeasure_callback(self, data):
-        self.beats_per_measure = data # listen to music input node, store number of beats per measure
-    
-    def beatTimes_callback(self, data):
-        self.beat_times = data        # listen to music input node, store beat times array
-    
-    def beatDurations_callback(self, data):
-        self.beat_durations = data    # listen to music input node, store beat durations array
+    def moves_callback(self, data):
+        tempMoveList = data.split(" ")
+        self.nodeMovesList = []
+        for i in tempMoveList:
+            temp = i.split(",")
+            self.nodeMovesList.append(temp)
 
-    
 
     def execute_dance(self):
     
@@ -67,10 +54,10 @@ class Driver(object):
         m = 1       # measure counter
         # time.sleep(beat_durations[0])   # buffer in order to improve synchronicity 
 
-        if self.pickup == 'y':       # adds pickup beat per user input (extra preceding beat before first measure)
+        if self.pickup == True:       # adds pickup beat per user input (extra preceding beat before first measure)
             y = self.beats_per_measure
             print('pickup measure')
-        elif self.pickup == 'n':
+        elif self.pickup == False:
             pass
 
         for i in self.beat_durations:        # records every measure change and every beat on time (can be subbed with real events)
@@ -80,44 +67,74 @@ class Driver(object):
             if self.beats_per_measure == 0:
                 if round_tempo <= 60:
                     print('beat',y,'(',x,') | dance move')
+                    #dance move:
+                    self.performMove(self.nodeMovesList[self.moveIndex])
+                    self.progressIndex()
                 elif round_tempo <= 120:
                     if y % 2 == 1:
                         print('beat',y,'(',x,') | dance move')
+                        #dance move:
+                        self.performMove(self.nodeMovesList[self.moveIndex])
+                        self.progressIndex()
                     else:
                         print('beat',y,'(',x,')')
+
                 elif round_tempo > 120:
                     if y % 3 == 1:
                         print('beat',y,'(',x,') | dance move')
+                        #dance move:
+                        self.performMove(self.nodeMovesList[self.moveIndex])
+                        self.progressIndex()
                     else:
                         print('beat',y,'(',x,')')
 
             elif self.beats_per_measure == 1:
                 if round_tempo <= 60:
                     print('beat',y,'(',x,') | dance move')
+                    #dance move:
+                    self.performMove(self.nodeMovesList[self.moveIndex])
+                    self.progressIndex()
                 elif round_tempo <= 120:
                     if x % 2 == 1:
                         print('beat',y,'(',x,') | dance move')
+                        #dance move:
+                        self.performMove(self.nodeMovesList[self.moveIndex])
+                        self.progressIndex()
                     else:
                         print('beat',y,'(',x,')')
+
                 elif round_tempo > 120:
                     if x % 3 == 1:
                         print('beat',y,'(',x,') | dance move')
+                        #dance move:
+                        self.performMove(self.nodeMovesList[self.moveIndex])
+                        self.progressIndex()
                     else:
                         print('beat',y,'(',x,')')
 
             elif self.beats_per_measure >= 2:
                 if round_tempo <= 60:
                     print('beat',y,'(',x,') | dance move')
+                    #dance move:
+                    self.performMove(self.nodeMovesList[self.moveIndex])
+                    self.progressIndex()
                 
                 elif round_tempo <= 120:
                     if y % 2 == 1:
                         if self.beats_per_measure % 2 == 1:
                             if y == self.beats_per_measure:
                                 print('beat',y,'(',x,')')
+
                             else:
                                 print('beat',y,'(',x,') | dance move')
+                                #dance move:
+                                self.performMove(self.nodeMovesList[self.moveIndex])
+                                self.progressIndex()
                         else:
                             print('beat',y,'(',x,') | dance move')
+                            #dance move:
+                            self.performMove(self.nodeMovesList[self.moveIndex])
+                            self.progressIndex()
                     else:
                         print('beat',y,'(',x,')')
                 
@@ -125,15 +142,21 @@ class Driver(object):
                     if self.beats_per_measure % 3 == 0:
                         if y % 3 == 1:
                             print('beat',y,'(',x,') | dance move')
+                            #dance move:
+                            self.performMove(self.nodeMovesList[self.moveIndex])
+                            self.progressIndex()
                         else:
                             print('beat',y,'(',x,')')
 
                     elif self.beats_per_measure < 6:
                         if y == 1:
                             print('beat',y,'(',x,') | dance move')
+                            #dance move:
+                            self.performMove(self.nodeMovesList[self.moveIndex])
+                            self.progressIndex()
                         else:
                             print('beat',y,'(',x,')')
-                    
+
                     elif self.beats_per_measure > 6:
                         num = self.beats_per_measure // 3     # number of dance moves
                         if y % 3 == 1:
@@ -141,6 +164,9 @@ class Driver(object):
                                 print('beat',y,'(',x,')')
                             else:
                                 print('beat',y,'(',x,') | dance move')
+                                #dance move:
+                                self.performMove(self.nodeMovesList[self.moveIndex])
+                                self.progressIndex()
                         else:
                             print('beat',y,'(',x,')')
 
@@ -151,6 +177,33 @@ class Driver(object):
             else:
                 y += 1
             time.sleep(i)       # wait for the beat to pass before printing the next beat to test synchronization with 
+
+    def performMove(self, move):
+
+        move_type = move[-1]
+        if(move_type == "line"):
+            self.drone.go_xyz_speed(0, move[0], move[1], move[2])
+        elif(move_type == "flip"):
+            direction = move[0]
+            if(direction == "right"):
+                self.drone.flip_right()
+            elif(direction == "left"):
+                self.drone.flip_left()
+            elif(direction == "backward"):
+                self.drone.flip_back()
+            else:
+                self.drone.flip_forward()
+        elif(move_type == "curve"):
+            pos1 = move[0]
+            pos2 = move[1]
+            self.drone.curve_xyz_speed(pos1[0], pos1[1], pos1[2], pos2[0], pos2[1], pos2[2], move[2])
+        elif(move_type == "3d"):
+            self.drone.go_xyz_speed(0,move[0], 0, move[1])
+
+    def progressIndex(self):
+        self.moveIndex += 1
+        if(self.moveIndex >= len(self.nodeMovesList)):
+            self.moveIndex = 0
 
 def __main__():
     driver = Driver()
