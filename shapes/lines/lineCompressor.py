d3ad4602aa6b53f7214da4a91dfb7d5b61dc890a
Alex Yung <superay190@gmail.com>
2022-08-04 15:40:11 -0400

    reorgnaized commit 13.5

--- shapes/lines/lineCompressor.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-
-for i in range(3):
-    img = Image.open('line{0}.jpg'.format(i))
-    new_size_ratio = .14
-    img = img.resize((int(img.size[0] * new_size_ratio), int(img.size[1] * new_size_ratio)), Image.Resampling.LANCZOS)
-    img.save('compline{0}.jpg'.format(i))
--- shapes/pixelConverter.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-import numpy as np
-
-for i in range(100):
-    im = Image.open('.png')
-    pixels = list(im.getdata())
-    width, height = im.size 
-    pixels = [pixels[i * width:(i + 1) * width] for i in range(height)]
-    np.savetxt("pixel_data.txt", pixels, delimiter=" ") 
